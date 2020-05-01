8bd232f0b4d20c14443643d38c5e327b31c4e0c5
hlongmore <henry@longmore.org>
2020-05-01 15:29:49 -0600

    Attempt to resolve incompatible versions
    Trying to install aiobotocore and pandas_redshift does not work. Neither pipenv nor poetry can find a solution, because aiobotocore constrains the botocore in a manner that doesn't let it work.
    
    ```
    aiobotocore==1.0.4
      - botocore [required: >=1.15.32,<1.15.33, installed: 1.16.1]
    pandas-redshift==2.0.3
      - boto3 [required: Any, installed: 1.13.1]
        - botocore [required: >=1.16.1,<1.17.0, installed: 1.16.1]
        - s3transfer [required: >=0.3.0,<0.4.0, installed: 0.3.3]
          - botocore [required: >=1.12.36,<2.0a.0, installed: 1.16.1]
    ```
    botocore can't be both < 1.15.33 and > 1.16.1.
    
    This is a hack of the required package version numbers in setup.py in hopes of letting pipenv find a solution.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # NOTE: When updating botocore make sure to update awscli/boto3 versions below
 install_requires = [
     # pegged to also match items in `extras_require`
-    'botocore>=1.15.32,<1.15.33',
+    'botocore>=1.15.32,<=1.16.1',
     'aiohttp>=3.3.1',
     'wrapt>=1.10.10',
     'aioitertools>=0.5.1',
@@ -20,7 +20,7 @@ def read(f):
 
 extras_require = {
     'awscli': ['awscli==1.18.32'],
-    'boto3': ['boto3==1.12.32'],
+    'boto3': ['boto3>=1.12,<=1.13.1'],
 }
 
 
